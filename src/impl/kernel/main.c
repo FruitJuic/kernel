9a5f2d462d532a88293c096552259f578cf0534b
Connor Townsend <83734352+FruitJuic@users.noreply.github.com>
2023-09-08 14:25:26 -0700

    Removed test code from the main kernel  function

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -3,7 +3,5 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Snake OS ");
-    print_addr("F", 5, 5);
-    print_str("initiation complete.");
+    print_str("Kernel succesfully loaded.");
 }
