420fd9bb52c0a48b11f94614286ab4c47e930a13
Connor Townsend <83734352+FruitJuic@users.noreply.github.com>
2023-09-08 14:42:26 -0700

    Switched main.c to use printf_str() instead of print_str().

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,6 +4,6 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
-    print_str("Kernel succesfully loaded. ");
+    printf_str("Kernel succesfully loaded.");
     print_str(keyboard_main());
 }
